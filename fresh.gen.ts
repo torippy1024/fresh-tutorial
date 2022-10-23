8f978e8bccfba970f48688a8303cf2355336b47a
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-23 09:42:25 +0900

    adding interactivity

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -8,11 +8,13 @@ import * as $1 from "./routes/about.tsx";
 import * as $2 from "./routes/api/joke.ts";
 import * as $3 from "./routes/api/random-uuid.ts";
 import * as $4 from "./routes/connpass/[keyword].tsx";
-import * as $5 from "./routes/github/[username].tsx";
-import * as $6 from "./routes/greet/[name].tsx";
-import * as $7 from "./routes/index.tsx";
-import * as $8 from "./routes/search.tsx";
-import * as $$0 from "./islands/Counter.tsx";
+import * as $5 from "./routes/countdown.tsx";
+import * as $6 from "./routes/github/[username].tsx";
+import * as $7 from "./routes/greet/[name].tsx";
+import * as $8 from "./routes/index.tsx";
+import * as $9 from "./routes/search.tsx";
+import * as $$0 from "./islands/Countdown.tsx";
+import * as $$1 from "./islands/Counter.tsx";
 
 const manifest = {
   routes: {
@@ -21,13 +23,15 @@ const manifest = {
     "./routes/api/joke.ts": $2,
     "./routes/api/random-uuid.ts": $3,
     "./routes/connpass/[keyword].tsx": $4,
-    "./routes/github/[username].tsx": $5,
-    "./routes/greet/[name].tsx": $6,
-    "./routes/index.tsx": $7,
-    "./routes/search.tsx": $8,
+    "./routes/countdown.tsx": $5,
+    "./routes/github/[username].tsx": $6,
+    "./routes/greet/[name].tsx": $7,
+    "./routes/index.tsx": $8,
+    "./routes/search.tsx": $9,
   },
   islands: {
-    "./islands/Counter.tsx": $$0,
+    "./islands/Countdown.tsx": $$0,
+    "./islands/Counter.tsx": $$1,
   },
   baseUrl: import.meta.url,
   config,
